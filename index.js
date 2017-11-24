a9167f9f484c90682deafb1507713cf6296c05e0
April Rivera <aprilcr87@gmail.com>
2017-11-24 19:44:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 var recipes = {Salad: 'romaine lettuce'};
+
+function updateObjectWithKeyAndValue(object, key, value){
+  object[key]= value;
+  return object;
+}
+updateObjectWithKeyAndValue(recipes, Tomatoes, '1 cup');
