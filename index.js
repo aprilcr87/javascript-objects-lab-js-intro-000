c6870cfbffc0619e128d03ccb6095703801ee0a2
April Rivera <aprilcr87@gmail.com>
2017-11-24 19:27:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-var saladRecipe = new Object('romaine lettuce': '2 cups', 'tomatoes': '1/2 cup', 'cheese': '1/4 cup');
+var saladRecipe = new Object(romaine lettuce: '2 cups', tomatoes: '1/2 cup', cheese: '1/4 cup');
